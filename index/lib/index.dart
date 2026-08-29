dfc4025715d90724e2d4764bb86b51db5cb1fd2e
YabseraBogale <yabserpython@gmail.com>
2026-08-29 18:48:12 +0300

    making present value class with it's method and fields

--- index/lib/index.dart
+++ index/lib/index.dart
@@ -1,3 +1,25 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:math';
+
+class PresentValue {
+  final num cashFlow;
+  final num rate;
+  final num period;
+  const PresentValue(this.cashFlow, this.rate, this.period);
+  @override
+  bool operator ==(Object other) {
+    identical(this, other) ||
+        other is PresentValue &&
+            runtimeType == other.runtimeType &&
+            cashFlow == other.cashFlow &&
+            rate == other.rate &&
+            period == other.period;
+    return super == other;
+  }
+
+  @override
+  int get hashCode => Object.hash(cashFlow, rate, period);
+
+  num calculate(num cashFlow, num rate, num period) {
+    return cashFlow / (pow((1 + rate), period));
+  }
 }
