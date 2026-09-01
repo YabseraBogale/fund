f8173569d0de13b6aab9a765b01a7b745c23e37a
YabseraBogale <yabserapython@gmail.com>
2026-09-01 19:11:52 +0300

    adding present value and removing balance

--- finance/finance.go
+++ finance/finance.go
@@ -1,6 +1,9 @@
 package finance
 
-import "math"
+import (
+	"errors"
+	"math"
+)
 
 type PresentValue struct {
 	CashFlow float64
@@ -9,4 +12,11 @@ type PresentValue struct {
 
 func (p *PresentValue) SimplePV(timePeriod int) float64 {
 	return p.CashFlow / math.Pow(1+p.Rate, float64(timePeriod))
+}
+
+func (p *PresentValue) GrowingPerpetuity(growth float64) (float64, error) {
+	if p.Rate < growth {
+		return -1, errors.New("garbage")
+	}
+	return p.CashFlow / (p.Rate - growth), nil
 }
